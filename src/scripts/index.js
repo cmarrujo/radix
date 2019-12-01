31a300c8c758be65974c232f9aa249b37dce22b5
Cesar Marrujo <marrujoc@gmail.com>
2019-12-01 15:43:29 -0800

    Sub pages created

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -45,7 +45,7 @@ class RDX {
     const rdxMenu = document.querySelector('.rdx-menu');
     const rdxDropdown = document.querySelector('.rdx-dropdown');
     const rdxSocial = document.querySelector('.rdx-social');
-    const rdxBgImage = document.querySelector('.rdx-dropdown--image');
+    // const rdxBgImage = document.querySelector('.rdx-dropdown--image');
     const rdxDropDownList = document.querySelector('.rdx-dropdown--list');
     let active = false;
 
@@ -56,7 +56,7 @@ class RDX {
           rdxLogo.setAttribute('data-active', `${active}`);
           rdxDropdown.setAttribute('data-active', `${active}`);
           rdxDropDownList.setAttribute('data-active', `${active}`);
-          rdxBgImage.setAttribute('data-active', `${active}`);
+          // rdxBgImage.setAttribute('data-active', `${active}`);
           rdxSocial && rdxSocial.setAttribute('data-active', `${active}`);
           setTimeout(() => {
             rdxMenu.setAttribute('data-active', `${active}`);        
@@ -64,13 +64,13 @@ class RDX {
         }else {
           active = false;
           rdxDropDownList.setAttribute('data-active', `${active}`);
-          rdxBgImage.setAttribute('data-active', `${active}`);
+          // rdxBgImage.setAttribute('data-active', `${active}`);
           setTimeout(() => {
             rdxDropdown.setAttribute('data-active', `${active}`);
             rdxLogo.setAttribute('data-active', `${active}`);
             rdxMenu.setAttribute('data-active', `${active}`);
             rdxSocial && rdxSocial.setAttribute('data-active', `${active}`);
-          }, 2000);
+          }, 1500);
         }
       });
     }
